3bae14f78e40f47945b05b07806177890fc155fa
fawoyera <femiawoyera@gmail.com>
2024-02-28 06:36:43 +0000

    update regex for matching text message log files

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:.*?\] \[to:.*?\] \[flags:.*?\]/).join
+file = ARGV[0]
+if File.exist?(file)
+	IO.foreach(file) {|line| 
+	token = line.scan(/(\[from:.*?\])|(\[to:.*?\])|(\[flags:.*?\])/).join
+	token_array = token.split(/:/)
+	puts "#{token_array[1].delete("][to")},#{token_array[2].delete("][flags")},#{token_array[3]}:#{token_array[4]}:#{token_array[5]}:#{token_array[6]}:#{token_array[7].delete("]")}"
+	}
+else
+	token = file.scan(/(\[from:.*?\])|(\[to:.*?\])|(\[flags:.*?\])/).join
+	token_array = token.split(/:/)
+	puts "#{token_array[1].delete("][to")},#{token_array[2].delete("][flags")},#{token_array[3]}:#{token_array[4]}:#{token_array[5]}:#{token_array[6]}:#{token_array[7].delete("]")}"
+end
