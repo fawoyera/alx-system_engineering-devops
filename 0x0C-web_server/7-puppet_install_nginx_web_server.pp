258945a09123c51b60f0799804277a164127d019
fawoyera <femiawoyera@gmail.com>
2024-05-02 04:57:03 -0700

    update puppet manifest for nginx web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,10 +2,5 @@
 
 exec { 'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "/server_name _/ r /dev/stdin" /etc/nginx/sites-available/default <<EOF
-
-        location /redirect_me {
-                return 301 https://www.alxafrica.com/;
-        }
-EOF ; sudo service nginx start'
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "/server_name _/s/;$/;\n\tlocation /redirect_me { return 301 https:\/\/www.alxafrica.com\/; }/ /etc/nginx/sites-available/default ; sudo service nginx start'
 }
