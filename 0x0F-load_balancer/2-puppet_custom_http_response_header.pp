bc75b57b770b0681b1f26076e83146dcfb5df3ed
fawoyera <femiawoyera@gmail.com>
2024-05-09 11:25:14 +0000

    add puppet manifest to configure nginx and add custom header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -21,13 +21,16 @@ file { '/var/www/html/index.nginx-debian.html':
   content => 'Hello World!',
 }
 
+# get the current hostname of server
+$hostname = scope('::hostname')
+
 # Edit configuration file
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0755',
-  content => '
+  content => "
 server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
@@ -37,6 +40,8 @@ server {
 
 	server_name _;
 
+        add_header X-Served-By ${hostname};
+
 	location /redirect_me {
 		return 301 https://www.alxafrica.com;
 	}
@@ -44,87 +49,6 @@ server {
 	location / {
 		try_files $uri $uri/ =404;
 	}
-}',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-
-# get the current hostname of server
-$hostname = scope('::hostname')
-
-# Add custom header configuration file
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => "
-user www-data;
-worker_processes auto;
-pid /run/nginx.pid;
-include /etc/nginx/modules-enabled/*.conf;
-
-events {
-        worker_connections 768;
-        # multi_accept on;
-}
-
-http {
-
-        ##
-        # Basic Settings
-        ##
-
-        add_header X-Served-By ${hostname};
-
-        sendfile on;
-        tcp_nopush on;
-        tcp_nodelay on;
-        keepalive_timeout 65;
-        types_hash_max_size 2048;
-        # server_tokens off;
-
-        # server_names_hash_bucket_size 64;
-        # server_name_in_redirect off;
-
-        include /etc/nginx/mime.types;
-        default_type application/octet-stream;
-
-
-        ##
-        # SSL Settings
-        ##
-
-        ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
-        ssl_prefer_server_ciphers on;
-
-        ##
-        # Logging Settings
-        ##
-
-        access_log /var/log/nginx/access.log;
-        error_log /var/log/nginx/error.log;
-
-        ##
-        # Gzip Settings
-        ##
-
-        gzip on;
-
-
-        # gzip_vary on;
-        # gzip_proxied any;
-        # gzip_comp_level 6;
-        # gzip_buffers 16 8k;
-        # gzip_http_version 1.1;
-
-        ##
-        # Virtual Host Configs
-        ##
-
-        include /etc/nginx/conf.d/*.conf;
-        include /etc/nginx/sites-enabled/*;
 }",
   require => Package['nginx'],
   notify  => Service['nginx'],
